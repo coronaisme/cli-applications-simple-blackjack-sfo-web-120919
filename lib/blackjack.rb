87fa2d30075f1a6d8997e4008d0d4199a8212b3a
Nick Corona <coronaisme@gmail.com>
2019-12-07 20:53:44 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,6 +31,10 @@ end
 def hit?(num)
   prompt_user
   get_user_input
+  if get_user_input == 's'
+    display_card_total(num)
+  else if get_user_input == 'h'
+    deal_card
 end
 
 def invalid_command
